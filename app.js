7e7b21c3f38a986f6d9f8d9d0f60654adf477404
ibrahimalsalim <lalsalm53@gmail.com>
2024-06-23 03:06:21 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+// app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/a", (req, res) => {
+    res.json({ m: "done" })
+})
+
+app.post("/b", (req, res) => {
+    res.json({ m: req.body.b })
+})
+
+
+app.listen(3000, () => console.log(`Server is running on 3000`));
